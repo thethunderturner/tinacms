cf616dec066891ad2e6941b4727dbf870d9c8c29
Matthew Biskas <matthewbiskas@gmail.com>
2025-12-19 06:28:07 +0100

    fix tests for clerk

--- packages/tinacms-clerk/src/index.ts
+++ packages/tinacms-clerk/src/index.ts
@@ -1,5 +1,5 @@
 import { createClerkClient } from '@clerk/backend';
-import type { IncomingMessage, ServerResponse } from 'http';
+import type { ServerResponse } from 'http';
 
 export const ClerkBackendAuthentication = ({
   secretKey,
@@ -12,57 +12,54 @@ export const ClerkBackendAuthentication = ({
   // Ensure the user is a member of the provided orgId
   orgId?: string;
 }) => {
-  const clerk = createClerkClient({ secretKey });
+  const clerkClient = createClerkClient({ secretKey });
 
   return {
-    isAuthorized: async (req: IncomingMessage, _res: ServerResponse) => {
+    isAuthorized: async (req: Request, _res: ServerResponse) => {
       const token = req.headers['authorization'];
       const tokenWithoutBearer = token?.replace('Bearer ', '').trim();
-
-      const requestState = await clerk.authenticateRequest(req, {
-        headerToken: tokenWithoutBearer,
+      const requestState = await clerkClient.authenticateRequest(req, {
+        acceptsToken: tokenWithoutBearer,
       });
 
       if (requestState.status === 'signed-in') {
-        const userId = requestState.toAuth().userId;
-
-        if (userId) {
-          const user = await clerk.users.getUser(userId);
-
-          if (orgId) {
-            const membershipList = (
-              await clerk.organizations.getOrganizationMembershipList({
-                organizationId: orgId,
-              })
-            ).map((x) => x.publicUserData?.userId);
-
-            if (!membershipList.includes(user.id)) {
-              return {
-                isAuthorized: false as const,
-                errorMessage:
-                  'User not authorized. Not a member of the provided organization.',
-                errorCode: 401,
-              };
-            }
-          }
-
-          const primaryEmail = user.emailAddresses.find(
-            (e) => e.id === user.primaryEmailAddressId
+        const user = await clerkClient.users.getUser(
+          requestState.toAuth().userId
+        );
+        if (orgId) {
+          // Get the list of member id's for the organization
+          const { data: memberships } =
+            await clerkClient.organizations.getOrganizationMembershipList({
+              organizationId: orgId,
+            });
+          const membershipList = memberships.map(
+            (x) => x.publicUserData?.userId
           );
-
-          if (primaryEmail && !allowList) {
-            return { isAuthorized: true as const };
-          }
-          if (primaryEmail && allowList?.includes(primaryEmail.emailAddress)) {
-            return { isAuthorized: true as const };
-          }
+          // if the user is not in the list, they are not authorized
+          if (!membershipList.includes(user.id))
+            return {
+              isAuthorized: false as const,
+              errorMessage:
+                'User not authorized. Not a member of the provided organization.',
+              errorCode: 401,
+            };
+        }
+        // if the user's email is not in the allowList, they are not authorized
+        const primaryEmail = user.emailAddresses.find(
+          ({ id }) => id === user.primaryEmailAddressId
+        );
+        if (primaryEmail && !allowList) {
+          return { isAuthorized: true as const };
+        }
+        // If they pass an allowList, check if the user is in it
+        if (primaryEmail && allowList?.includes(primaryEmail.emailAddress)) {
+          return { isAuthorized: true as const };
         }
       }
 
       if (requestState.reason === 'unexpected-error') {
         console.error(requestState.message);
       }
-
       return {
         isAuthorized: false as const,
         errorMessage: 'User not authorized',
